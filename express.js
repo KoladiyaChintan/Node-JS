28a7ae963f0f8cfcce9fe7cb998e887d91a8d38e
koladiyachintan <chintankoladiya4471@gmail.com>
2022-04-18 15:22:34 +0530

    create express server

--- express.js
+++ express.js
@@ -1,26 +1,12 @@
-const express = require('express')
-const mysql = require('mysql')
+const express = require("express");
+const res = require("express/lib/response");
 
 var app = express()
 
-var connetion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'world',
-})
-
 app.get('/', function (req, res) {
-    let sql = "SELECT * FROM CITY";
-    connetion.query(sql, function (err, result) {
-        if (err) throw err;
-        res.send(result)
-    })
+    res.send("server created");
 })
 
 app.listen(8000, function () {
-    console.log("server listening on port 8000");
-    connetion.connect(function () {
-        console.log("database connected");
-    })
+    console.log("server created");
 })
