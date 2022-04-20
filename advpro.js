610c60154d3f7151ee24481e6984d9bb9f9da465
koladiyachintan <chintankoladiya4471@gmail.com>
2022-04-20 14:32:08 +0530

    initial commit

--- advpro.js
+++ advpro.js
@@ -1,22 +1,22 @@
 const p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("promise 1"), 2000
-    });
+        resolve("promise 1")
+    }, 2000);
 })
 
 const p2 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("promise 2"), 2000
-    });
+        resolve("promise 2")
+    }, 2000);
 })
 
 const p3 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        reject("promise 3"), 2000
-    })
+        reject("promise 3")
+    }, 2000)
 })
 
-Promise.all([p1, p2, p3]).then((data) => {
+Promise.race([p1, p2, p3]).then((data) => {
     console.log(data);
 }).catch((err) => {
     console.log("error" + err);
